9311391d603c7c84b1f3c06fbe3f3ead27c0f2a7
mamaredo_d <11kakeru30@gmail.com>
2021-06-22 23:07:49 +0900

    formatter等の設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-  ],
+  plugins: ['vue', 'prettier'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+  },
 }
